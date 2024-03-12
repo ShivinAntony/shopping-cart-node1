68f3c5dd553efb72abf4fcca535945a35cd5ea0b
Shivin Antony <shivinantony087@gmail.com>
2024-03-12 14:29:31 +0530

    edit-product

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -1,7 +1,8 @@
 const { resolve, reject } = require('promise');
 var db = require('../config/connection');
-var collection = require('../config/collections')
-var objectId = require('mongodb').ObjectId
+var collection = require('../config/collections');
+const { response } = require('../app');
+const ObjectId =require('mongodb').ObjectId;
 module.exports = {
     addProduct: (product, callback) => {
         console.log(product);
@@ -19,14 +20,41 @@ module.exports = {
     deleteProduct: (proId) => {
         return new Promise((resolve, reject) => {
             console.log(proId);
-            console.log(new objectId(proId));
+            console.log( new ObjectId(proId));
 
-            db.get().collection(collection.PRODUCT_COLLECTION).deleteOne({ _id: new objectId(proId) }).then((response) => {
+            db.get().collection(collection.PRODUCT_COLLECTION).deleteOne({ _id: new ObjectId(proId) }).then((response) => {
                 resolve(response);
             }).catch(err => {
                 reject(err);
             });
         });
-    }
+    },
+
+
+    getProductDetails: (proId) => {
+        return new Promise( (resolve, reject) => {
+            db.get().collection(collection.PRODUCT_COLLECTION).findOne({ _id:new  ObjectId(proId) }).then((product) => {
+                resolve(product)
+            })
+        })
+   },
+     updateProduct:(proId,proDetails)=>{
+        return new Promise((resolve,reject)=>{
+            db.get().collection(collection.PRODUCT_COLLECTION)
+            .updateOne({_id: new ObjectId(proId)},{
+                $set:{
+                    Name: proDetails.Name,
+                    Description :proDetails.Description,
+                    Price : proDetails.Price,
+                    Category: proDetails.Category
+                }
+            }).then((response)=>{
+                resolve(response);
+            }).catch((err)=>{
+                reject(err)
+            })
+        })
+     }
+
 
 };
--- routes/admin.js
+++ routes/admin.js
@@ -1,49 +1,81 @@
 var express = require('express');
-var multer=require('multer');
-var path =require('path');
+var multer = require('multer');
+var path = require('path');
 var router = express.Router();
-var productHelper =require('../helpers/product-helpers');
-const productHelpers = require('../helpers/product-helpers');
+var productHelper = require('../helpers/product-helpers');
+var productHelpers = require('../helpers/product-helpers');
 const { response } = require('../app');
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-   
-   productHelpers.getAllProducts().then((products)=>{
-    console.log(products)
-    res.render('admin/view-products', { admin: true, products })
+
+   productHelpers.getAllProducts().then((products) => {
+      console.log(products)
+      res.render('admin/view-products', { admin: true, products })
    });
-  
+
 });
 router.get('/add-product', function (req, res) {
-  res.render('admin/add-product')
+   res.render('admin/add-product')
 })
 router.post('/add-product', (req, res) => {
-  console.log(req.body)
-  console.log(req.files.image)
+   console.log(req.body)
+   console.log(req.files.Image)
 
-  productHelpers.addProduct(req.body,(id)=>{
-     let image=req.files.image
-     console.log(id);
-     image.mv('./public/product-images/'+id+'.jpg',(err,done)=>{
-       if(!err){
-        res.render("admin/add-product");
-       }else{
-        console.log(err);
-       }
-    });
-   
-     
-   
- });
+   productHelpers.addProduct(req.body, (id) => {
+      let image = req.files.Image
+      console.log(id);
+      image.mv('./public/product-images/' + id + '.jpg', (err, done) => {
+         if (!err) {
+            res.render("admin/add-product");
+         } else {
+            console.log(err);
+         }
+      });
+
+   });
 });
-router.get('/delete-product/:id',(req,res)=>{
-   let proId=req.params.id;
+router.get('/delete-product/:id', (req, res) => {
+   let proId = req.params.id;
    console.log(proId);
-   productHelpers.deleteProduct(proId).then((response)=>{
+   productHelpers.deleteProduct(proId).then((response) => {
       res.redirect('/admin/');
-   })
-   
-})
+   });
+
+});
+
+router.get('/edit-product/:id', async (req, res) => {
+   let product = await productHelpers.getProductDetails(req.params.id);
+   console.log(product);
+   res.render('admin/edit-product', { product });
+});
+
+router.post('/edit-product/:id', (req, res) => {
+   console.log(req.params.id);
+   let id = req.params.id;
+
+   productHelpers.updateProduct(id, req.body).then(() => {
+      // Product update successful, now check for file upload
+      if (req.files && req.files.Image) {
+         let image = req.files.Image;
+         image.mv('./public/product-images/' + id + '.jpg', (err) => {
+            if (err) {
+               console.error(err);
+               res.status(500).send('Error uploading image');
+            } else {
+               res.redirect('/admin');
+            }
+         });
+      } else {
+         res.redirect('/admin');
+      }
+   }).catch((error) => {
+      console.error(error);
+      res.status(500).send('Internal Server Error');
+   });
+});
+
+
+
 
 module.exports = router; 
