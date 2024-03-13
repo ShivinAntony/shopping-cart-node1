295dff8036daaabb2befeab31480ad921bcbc8fc
Shivin Antony <shivinantony087@gmail.com>
2024-03-13 16:41:34 +0530

    Editimage-AddtoCart

--- helpers/user-helpers.js
+++ helpers/user-helpers.js
@@ -1,40 +1,94 @@
 var db = require('../config/connection');
-var collection=require('../config/collections')
-const bcrypt =require('bcrypt');
-const { resolve } = require('promise');
-module.exports={
-    doSignup:(userData)=>{
-        return new Promise(async(resolve,reject)=>{
-        userData.Password=await bcrypt.hash(userData.Password,10,)
-        db.get().collection(collection.USER_COLLECTION).insertOne(userData).then((data)=>{
-            resolve(data.insertedId)
-        })
-       
+var collection = require('../config/collections')
+const bcrypt = require('bcrypt');
+const { resolve, reject } = require('promise');
+const { response } = require('express');
+const ObjectId = require('mongodb').ObjectId;
+module.exports = {
+    doSignup: (userData) => {
+        return new Promise(async (resolve, reject) => {
+            userData.Password = await bcrypt.hash(userData.Password, 10,)
+            db.get().collection(collection.USER_COLLECTION).insertOne(userData).then((data) => {
+                resolve(data.insertedId)
+            })
+
         })
     },
-    doLogin:(userData)=>{
-        return new Promise(async(resolve,reject)=>{
+    doLogin: (userData) => {
+        return new Promise(async (resolve, reject) => {
             let loginStatus = false
-            let response= {}
-            let user=await db.get().collection(collection.USER_COLLECTION).findOne({Email:userData.Email})
-           
-            
-            if(user){
-                bcrypt.compare(userData.Password,user.Password).then((status)=>{
-                  if(status){
-                    console.log("login success")
-                    response.user=user
-                    response.status=true
-                    resolve(response)
-                  }else{
-                    console.log('login failed')
-                    resolve({status:false})
-                  }
+            let response = {}
+            let user = await db.get().collection(collection.USER_COLLECTION).findOne({ Email: userData.Email })
+
+
+            if (user) {
+                bcrypt.compare(userData.Password, user.Password).then((status) => {
+                    if (status) {
+                        console.log("login success")
+                        response.user = user
+                        response.status = true
+                        resolve(response)
+                    } else {
+                        console.log('login failed')
+                        resolve({ status: false })
+                    }
                 })
-            }else{
+            } else {
                 console.log('login failed')
-                resolve({status:false})
+                resolve({ status: false })
             }
+        })
+    },
+    addToCart: (proId, userId) => {
+        return new Promise(async (resolve, reject) => {
+            let userCart = await db.get().collection(collection.CART_COLLECTION).findOne({ user: new ObjectId(userId) })
+            if (userCart) {
+                db.get().collection(collection.CART_COLLECTION)
+                    .updateOne({ user: new ObjectId(userId) },
+                        {
+                            
+                                $push: { products: new ObjectId(proId) }
+                            
+                        }
+                    ).then((response) => {
+                        resolve()
+                    })
+            } else {
+                let cartObj = {
+                    user: new ObjectId(userId),
+                    products: [new ObjectId(proId)]
+                }
+                db.get().collection(collection.CART_COLLECTION).insertOne(cartObj).then((response) => {
+                    resolve()
+                })
+            }
+        })
+    },
+    getCartProducts:(userId)=>{
+        return new Promise(async(resolve,reject)=>{
+            let cartItems=await db.get().collection(collection.CART_COLLECTION).aggregate([
+                {
+                    $match:{user: new ObjectId(userId)}
+                },
+                {
+                    $lookup:{
+                        from:collection.PRODUCT_COLLECTION,
+                        let:{prodList:'$products'},
+                        pipeline:[
+                            {
+                                $match:{
+                                    $expr:{
+                                        $in:['$_id',"$$prodList"]
+                                    }
+                                }
+
+                            }
+                        ],
+                        as:'cartItems'
+                    }
+                }
+            ]).toArray()
+            resolve(cartItems[0].cartItems)
         })
     }
 }
