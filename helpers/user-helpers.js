d4caed72f467a2cb7e363721e03ed26caee7036b
Shivin Antony <shivinantony087@gmail.com>
2024-03-14 14:36:43 +0530

    Add to cart count

--- helpers/user-helpers.js
+++ helpers/user-helpers.js
@@ -90,5 +90,15 @@ module.exports = {
             ]).toArray()
             resolve(cartItems[0].cartItems)
         })
+    },
+    getCartCount:(userId)=>{
+        return new Promise(async(resolve,reject)=>{
+            let count=0
+            let cart=await db.get().collection(collection.CART_COLLECTION).findOne({user:new ObjectId(userId)})
+            if(cart){
+                count=cart.products.length
+            }
+            resolve(count)
+        })
     }
 }
--- routes/user.js
+++ routes/user.js
@@ -10,12 +10,17 @@ const verifyLogin = (req, res, next) => {
   }
 }
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/', async function (req, res, next) {
   let user = req.session.user
   console.log(user);
+  let cartCount=null
+  if(req.session.user){
+   cartCount = await userHelpers.getCartCount(req.session.user._id)
+  }
+  
   productHelpers.getAllProducts().then((products) => {
 
-    res.render('user/view-products', { products, user })
+    res.render('user/view-products', { products, user, cartCount })
   })
 });
 
@@ -57,15 +62,16 @@ router.get('/logout', (req, res) => {
   res.redirect('/')
 })
 
-router.get('/cart', verifyLogin,async (req, res) => {
-  let products=await userHelpers.getCartProducts(req.session.user._id)
+router.get('/cart', verifyLogin, async (req, res) => {
+  let products = await userHelpers.getCartProducts(req.session.user._id)
   console.log(products);
-  res.render('user/cart')
+  res.render('user/cart', { products, user: req.session.user })
 })
 
-router.get('/add-to-cart/:id',(req,res)=>{
-  userHelpers.addToCart(req.params.id,req.session.user._id).then(()=>{
-    res.redirect('/')
+router.get('/add-to-cart/:id', (req, res) => {
+  console.log("api call");
+  userHelpers.addToCart(req.params.id, req.session.user._id).then(() => {
+    res.json({status:true})
   })
 })
 
